68aa0e8c93b47dda5470297b9fa5883a60214d81
JorgeQuinteros <quinterosorellana04@gmail.com>
2019-05-27 17:28:57 +0200

    [ADD] description in team and category

--- helpdesk/models/helpdesk_category.py
+++ helpdesk/models/helpdesk_category.py
@@ -3,6 +3,7 @@ from odoo import api, fields, models
 class HelpdeskCategory(models.Model):
 
     _name = 'helpdesk.ticket.category'
+    _description = 'Helpdesk Ticket Category'
 
     active = fields.Boolean(string='Active')
     name = fields.Char(string='Name', required=True)
--- helpdesk/models/helpdesk_team.py
+++ helpdesk/models/helpdesk_team.py
@@ -4,6 +4,7 @@ from odoo import api, fields, models
 class HelpdeskTeam(models.Model):
 
     _name = 'helpdesk.ticket.team'
+    _description = 'Helpdesk Ticket Team'
 
     name = fields.Char(string='Name', required=True)
     user_ids = fields.Many2many(comodel_name='res.users', string='Members')
