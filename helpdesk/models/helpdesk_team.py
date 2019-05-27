0742e5192cb17d580b9b4147ea7324882385ee96
JorgeQuinteros <quinterosorellana04@gmail.com>
2019-05-27 17:02:42 +0200

    [ADD] many2many field team to categories

--- helpdesk/models/helpdesk_team.py
+++ helpdesk/models/helpdesk_team.py
@@ -7,5 +7,6 @@ class HelpdeskTeam(models.Model):
 
     name = fields.Char(string='Name', required=True)
     user_ids = fields.Many2many(comodel_name='res.users', string='Members')
+    category_ids = fields.Many2many(comodel_name='helpdesk.ticket.category', string='Category')
 
 
