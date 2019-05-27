35af5953d8ca43638258cb5f985203c99bdf1b41
carlosDomatix <carlos@domatix.com>
2019-05-27 18:15:37 +0200

    [ADD] Stage model and data (#16)

--- helpdesk_mgmt/models/helpdesk_ticket.py
+++ helpdesk_mgmt/models/helpdesk_ticket.py
@@ -7,11 +7,20 @@ class HelpdeskTicket(models.Model):
     _order = 'number desc'
     _inherit = ['portal.mixin', 'mail.thread', 'mail.activity.mixin']
 
+    def _get_default_stage_id(self):
+        return self.env['helpdesk.ticket.stage'].search([], limit=1).id
+
     number = fields.Char(string='Ticket number', readonly=True)
     name = fields.Char(string='Title', required=True)
     description = fields.Text(required=True)
-    user_id = fields.Many2one('res.users', string='Assigned user', default=lambda self: self.env.user)
-    state = fields.Many2one('helpdesk.ticket.state', string='State') #TODO: default
+    user_id = fields.Many2one(
+        'res.users',
+        string='Assigned user',
+        default=lambda self: self.env.user)
+    stage_id = fields.Many2one(
+        'helpdesk.ticket.stage',
+        string='State',
+        default=_get_default_stage_id)
     partner_id = fields.Many2one('res.partner')
     partner_name = fields.Char()
     partner_email = fields.Char()
@@ -27,8 +36,10 @@ class HelpdeskTicket(models.Model):
         default=lambda self: self.env['res.company']._company_default_get(
             'helpdesk.ticket')
     )
-    channel_id = fields.Many2one('helpdesk.ticket.channel', string='Channel')
-    category_id = fields.Many2one('helpdesk.ticket.category', string='Category')
+    channel_id = fields.Many2one('helpdesk.ticket.channel',
+                                 string='Channel')
+    category_id = fields.Many2one('helpdesk.ticket.category',
+                                  string='Category')
     team_id = fields.Many2one('helpdesk.ticket.team')
     priority_id = fields.Selection(selection=[
         ('0', _('Low')),
@@ -36,6 +47,7 @@ class HelpdeskTicket(models.Model):
         ('2', _('High')),
         ('3', _('Very High')),
     ], string='Priority', default='medium')
-    attachment_ids = fields.One2many('ir.attachment', 'res_id', domain=[('res_model', '=', 'website.support.ticket')],
-                                     string="Media Attachments")
-
+    attachment_ids = fields.One2many(
+        'ir.attachment', 'res_id',
+        domain=[('res_model', '=', 'website.support.ticket')],
+        string="Media Attachments")
