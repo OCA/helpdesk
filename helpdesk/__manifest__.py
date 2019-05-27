3312b7068f6e48a1f781f5f1745483e219192ccf
Eduardo Magdalena <emagdalena@c2i.es>
2019-05-27 20:08:39 +0200

    [ADD] Readme (#18)
    * [ADD] Readme
    
    * Generate readme with OCA Maintainer Tools
    [FIX] 'heldesk' in __manifest__.py
    
    * [ADD] Stage model and data (#16)
    
    * [FIX] Flake8 and naming conventions
    [FIX] Remove ACSONE
    [FIX] Build error with portal.mixing. This will be added in a second module
    
    * [FIX] Fix travis errors

--- helpdesk/__manifest__.py
+++ helpdesk/__manifest__.py
@@ -6,12 +6,14 @@
         Helpdesk""",
     'version': '11.0.1.0.0',
     'license': 'AGPL-3',
-    'author': 'Odoo Community Association (OCA), ACSONE SA/NV, Domatix',
+    'author': 'Odoo Community Association (OCA), Domatix',
     'website': 'https://github.com/OCA/helpdesk',
-    'depends': [],
+    'depends': [
+        'mail',
+    ],
     'data': [
         'data/helpdesk_data.xml',
-        'views/heldesk_team_views.xml'
+        'views/helpdesk_team_views.xml'
     ],
     'demo': [],
 }
