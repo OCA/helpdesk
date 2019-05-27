86a0cbf81652d1a3b60cfae0a01ef5eeeff19171
Aitor Bouzas <bouzasnaveira@gmail.com>
2019-05-27 12:52:07 +0200

    [ADD] Initial commit
    * Add ticket model and its fields
    * Adding readme with different tasks

---
+++ helpdesk_mgmt/__manifest__.py
@@ -0,0 +1,14 @@
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Helpdesk',
+    'summary': """
+        Helpdesk""",
+    'version': '11.0.1.0.0',
+    'license': 'AGPL-3',
+    'author': 'Odoo Community Association (OCA), ACSONE SA/NV',
+    'website': 'https://github.com/OCA/helpdesk',
+    'depends': [],
+    'data': [],
+    'demo': [],
+}
