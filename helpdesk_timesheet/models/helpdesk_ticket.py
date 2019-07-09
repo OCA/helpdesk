50a61439c8fcd2fcc5f81d1f9bfa259344fdb33a
notze <denny123>
2019-07-09 11:28:22 +0200

    [FIX] improved if condition liek suggested in review

--- helpdesk_timesheet/models/helpdesk_ticket.py
+++ helpdesk_timesheet/models/helpdesk_ticket.py
@@ -6,26 +6,30 @@ from odoo.exceptions import ValidationError
 
 
 class HelpdeskTicket(models.Model):
-    _inherit = 'helpdesk.ticket'
+    _inherit = "helpdesk.ticket"
 
     project_id = fields.Many2one(
-        comodel_name='project.project',
-        string="Project",
+        comodel_name="project.project", string="Project"
     )
     timesheet_ids = fields.One2many(
-        comodel_name='account.analytic.line',
-        inverse_name='helpdesk_ticket_id',
+        comodel_name="account.analytic.line",
+        inverse_name="helpdesk_ticket_id",
         string="Timesheet",
     )
 
     @api.multi
     def write(self, values):
         res = super(HelpdeskTicket, self).write(values)
-        if values.get('stage_id'):
-            if self.stage_id.timesheet_required:
-                if len(self.timesheet_ids) == 0:
-                    raise ValidationError(
-                        _("Timesheet Required: "
-                            "Please provide a Timesheet\
-                          to reach this ticket stage"))
+        if (
+            values.get("stage_id")
+            and self.stage_id.timesheet_required
+            and not self.timesheet_ids
+        ):
+            raise ValidationError(
+                _(
+                    "Timesheet Required: "
+                    "Please provide a Timesheet\
+                          to reach this ticket stage"
+                )
+            )
         return res
