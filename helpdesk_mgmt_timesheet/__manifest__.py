630e5077f1770a3ebfd14f8f635e9c36452a2b19
notze <denny123>
2019-07-10 11:08:07 +0200

    [FIX] FIXED some OCA Sytle wishes :)

--- helpdesk_mgmt_timesheet/__manifest__.py
+++ helpdesk_mgmt_timesheet/__manifest__.py
@@ -4,13 +4,16 @@
         Add Timesheets to the Helpdesk""",
     "category": "After-Sales",
     "version": "11.0.1.0.0",
-    "depends": ["helpdesk_mgmt", "hr_timesheet"],
+    "depends": ["helpdesk_mgmt",
+                "hr_timesheet"
+    ],
     "data": [
         "views/helpdesk_view.xml",
         "views/hr_timesheet_view.xml",
         "views/helpdesk_ticket_stage_view.xml",
     ],
-    "author": "Agent ERP GmbH, " "Odoo Community Association (OCA)",
+    "author": "Agent ERP GmbH, "
+              "Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/helpdesk",
     "license": "AGPL-3",
     "installable": True,
