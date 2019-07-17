651e9a6c77ffc6afe8d772353fe1f9a631a10021
Pierrick Brun <pierrick.brun@akretion.com>
2019-07-17 16:05:29 +0200

    [IMP] allow reopen from comments on non-prod env

--- helpdesk_reopen/models/mail_message.py
+++ helpdesk_reopen/models/mail_message.py
@@ -9,9 +9,28 @@ from odoo import models, api
 class MailMessage(models.Model):
     _inherit = 'mail.message'
 
+    def is_production_env(self):
+        """ Used to determine how we will treat comments.
+        It is useful because we can't (easily) mock
+        incoming emails in testing environnements
+        It needs to be implemented"""
+        return False
+
+    @api.model
+    def is_reopener_message(self, vals):
+        if not vals.get('model') == 'helpdesk.ticket':
+            return False
+        if vals.get('message_type') == 'notification':
+            return False
+        # We need to use comments in non-prod env for testing purposes
+        if self.is_production_env() and vals.get('message_type') == 'comment':
+            return False
+        return True
+
+
     @api.model
     def create(self, vals):
-        if vals.get('message_type') != 'notification' and vals.get('model') == 'helpdesk.ticket':
+        if self.is_reopener_message(vals):
             ticket = self.env["helpdesk.ticket"].search([('id', '=', vals.get('res_id'))])
             if ticket:
                 if ticket.stage_id.is_close:
