09634b98d158a9c96a91273404612dfc9eefdc85
emagdalenaC2i <emagdalena@c2i.es>
2019-10-18 15:25:50 +0200

    [FIX] pre-commit

--- helpdesk_mgmt/controllers/main.py
+++ helpdesk_mgmt/controllers/main.py
@@ -18,7 +18,7 @@ class HelpdeskTicketController(http.Controller):
                 values[field_name] = field_value
         ticket = (
             http.request.env["helpdesk.ticket"]
-            .with_user(user)
+            .with_user()
             .search([("id", "=", values["ticket_id"])])
         )
         ticket.stage_id = values.get("stage_id")
@@ -48,21 +48,21 @@ class HelpdeskTicketController(http.Controller):
             "name": kw.get("subject"),
             "attachment_ids": False,
             "channel_id": request.env["helpdesk.ticket.channel"]
-            .with_user(user)
+            .with_user()
             .search([("name", "=", "Web")])
             .id,
             "partner_id": request.env["res.partner"]
-            .with_user(user)
+            .with_user()
             .search([("name", "=", kw.get("name")), ("email", "=", kw.get("email"))])
             .id,
         }
-        new_ticket = request.env["helpdesk.ticket"].with_user(user).create(vals)
+        new_ticket = request.env["helpdesk.ticket"].with_user().create(vals)
         new_ticket.message_subscribe(partner_ids=request.env.user.partner_id.ids)
         if kw.get("attachment"):
             for c_file in request.httprequest.files.getlist("attachment"):
                 data = c_file.read()
                 if c_file.filename:
-                    request.env["ir.attachment"].with_user(user).create(
+                    request.env["ir.attachment"].with_user().create(
                         {
                             "name": c_file.filename,
                             "datas": base64.b64encode(data),
