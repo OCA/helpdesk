e7ccea5b9fc117eff60d645baf5b21eb11a4d781
Ananias Filho <ananias@kmee.com.br>
2019-10-23 21:41:58 -0300

    [New] Base for website_helpdesk

---
+++ website_helpdesk/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2019 KMEE
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Website Helpdesk',
+    'summary': """
+        Create helpdesk tickets from website portal""",
+    'version': '12.0.1.0.0',
+    'license': 'AGPL-3',
+    'author': 'KMEE,Odoo Community Association (OCA)',
+    'website': 'https://www.kmee.com.br/',
+    'depends': [
+    ],
+    'data': [
+    ],
+    'demo': [
+    ],
+}
