82e739e354919298983265349bd8983fc6c8dbd3
Maxime Chambreuil <mchambreuil@opensourceintegrators.com>
2019-12-18 14:11:58 -0600

    [ADD] helpdesk_type

---
+++ helpdesk_type/models/helpdesk_ticket_type.py
@@ -0,0 +1,16 @@
+# Copyright (C) 2019 Konos
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+from odoo import fields, models
+
+
+class HelpdeskType(models.Model):
+    """Helpdesk Type"""
+    _name = "helpdesk.ticket.type"
+    _description = 'Helpdesk Ticket Type'
+    _order = "name asc"
+
+    name = fields.Char('Name', required=True)
+    team_ids = fields.Many2many(
+        'helpdesk.ticket.team',
+        string='Teams',
+        help="Helpdesk teams allowed to use this type.")
