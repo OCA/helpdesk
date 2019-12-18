a55204732662e45c672093fdf330ef9252e12658
Maxime Chambreuil <mchambreuil@opensourceintegrators.com>
2019-12-18 17:55:40 -0600

    [IMP] helpdesk_motive

--- helpdesk_motive/models/helpdesk_ticket.py
+++ helpdesk_motive/models/helpdesk_ticket.py
@@ -1,7 +1,7 @@
 # Copyright (c) 2019 Open Source Integrators
 # Copyright (C) 2019 Konos
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class HelpdeskTicket(models.Model):
@@ -9,3 +9,9 @@ class HelpdeskTicket(models.Model):
 
     motive_id = fields.Many2one(
         "helpdesk.ticket.motive", string="Motive", help="Motive")
+
+    @api.multi
+    @api.onchange('team_id', 'user_id')
+    def _onchange_dominion_user_id(self):
+        super()._onchange_dominion_user_id()
+        self.motive_id = False
