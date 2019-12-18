14c85bd085d718ae59b98d9b81f099a397487470
Maxime Chambreuil <mchambreuil@opensourceintegrators.com>
2019-12-18 14:11:58 -0600

    [ADD] helpdesk_type

---
+++ helpdesk_type/models/helpdesk_ticket_team.py
@@ -0,0 +1,15 @@
+# Copyright (c) 2019 Open Source Integrators
+# Copyright (C) 2019 Konos
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+from odoo import fields, models
+
+
+class HelpdeskTeam(models.Model):
+    _inherit = "helpdesk.ticket.team"
+
+    type_ids = fields.Many2many(
+        "helpdesk.ticket.type",
+        string="Ticket Type",
+        help="Ticket Types the team will use. This team's tickets will only "
+        "be able to use those types.",
+    )
