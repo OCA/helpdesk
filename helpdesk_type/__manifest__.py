5a50720fb1085b526847afc97a60af35b173a16e
Maxime Chambreuil <mchambreuil@opensourceintegrators.com>
2019-12-19 08:42:10 -0600

    [FIX] helpdesk_type

--- helpdesk_type/__manifest__.py
+++ helpdesk_type/__manifest__.py
@@ -3,10 +3,10 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    "name": "Helpdesk Motive",
+    "name": "Helpdesk Ticket Type",
     "version": "12.0.1.0.0",
     "license": "AGPL-3",
-    "summary": "Keep the motive ",
+    "summary": "Add a type to your tickets",
     "author": "Konos, "
               "Open Source Integrators, "
               "Odoo Community Association (OCA)",
