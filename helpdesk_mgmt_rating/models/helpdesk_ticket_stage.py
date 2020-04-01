5dc46247a8bfa7135b43c6760921e5d89964bbad
cubells <vicent@vcubells.net>
2020-04-01 18:35:01 +0200

    [IMP] helpdesk_mgmt_rating: black, isort, prettier

--- helpdesk_mgmt_rating/models/helpdesk_ticket_stage.py
+++ helpdesk_mgmt_rating/models/helpdesk_ticket_stage.py
@@ -1,12 +1,13 @@
-from odoo import models, fields
+from odoo import fields, models
 
 
 class HelpdeskTicketStage(models.Model):
-    _inherit = 'helpdesk.ticket.stage'
+    _inherit = "helpdesk.ticket.stage"
 
     rating_mail_template_id = fields.Many2one(
-        'mail.template',
+        comodel_name="mail.template",
         string="Rating Email Template",
-        domain=[('model', '=', 'helpdesk.ticket')],
+        domain=[("model", "=", "helpdesk.ticket")],
         help="If set, an email will be sent to the customer  "
-             "with a rating survey when the ticket reaches this stage.")
+        "with a rating survey when the ticket reaches this stage.",
+    )
