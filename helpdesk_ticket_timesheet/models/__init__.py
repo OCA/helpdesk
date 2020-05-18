c810fd175d4e6d27b33ab4a65665306f8a6d4fda
Jorge Luis Quinteros <jquinteros@sdi.es>
2020-05-18 16:35:48 +0200

    [FIX] Cleancode Flake-8

--- helpdesk_ticket_timesheet/models/__init__.py
+++ helpdesk_ticket_timesheet/models/__init__.py
@@ -1,5 +1,6 @@
-# -*- coding: utf-8 -*-
-
+###############################################################################
+# For copyright and license notices, see __manifest__.py file in root directory
+###############################################################################
 from . import account_analytic_line
 from . import ticket_timesheet
 from . import helpdesk_ticket_team
