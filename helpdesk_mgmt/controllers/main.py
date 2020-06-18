e2fbfdf6ef58058ef1d5436ee614b53a4ad2c763
David James <david@djdc.net.au>
2020-06-18 20:00:07 +1000

    helpdesk_mgmt: fix error when submitting a ticket with an attachment
    Fixes #138 which was caused by deprecated field datas_fname being included in the attachment values

--- helpdesk_mgmt/controllers/main.py
+++ helpdesk_mgmt/controllers/main.py
@@ -69,7 +69,6 @@ class HelpdeskTicketController(http.Controller):
                         {
                             "name": c_file.filename,
                             "datas": base64.b64encode(data),
-                            "datas_fname": c_file.filename,
                             "res_model": "helpdesk.ticket",
                             "res_id": new_ticket.id,
                         }
