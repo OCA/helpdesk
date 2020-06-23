65c674899be30b7795c9697125730b35c6ad1af2
oscars8a <oscars.soto.ochoa@gmail.com>
2020-06-23 11:58:45 +0200

    [FIX] bloated headers

--- helpdesk_mgmt_timesheet/__manifest__.py
+++ helpdesk_mgmt_timesheet/__manifest__.py
@@ -1,22 +1,8 @@
-##############################################################################
 #    Copyright (C) 2020 Aresoltec Canarias <www.aresoltec.com>
 #    Copyright (C) 2020 Punt Sistemes <www.puntsistemes.es.es>
 #    Copyright (C) 2020 SDi Soluciones Digitales <www.sdi.es>
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
 {
     'name': 'Helpdesk Ticket Timesheet',
     'summary': 'Add HR Timesheet to the tickets for Helpdesk Management.',
