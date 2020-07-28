99f7d734efa4799403957c485c206a8fe1bbf982
David Alonso (Solvos) <david.alonso@solvos.es>
2020-07-28 09:09:40 +0200

    [ADD] helpdesk_mgmt_timesheet: original files from PR #121

---
+++ helpdesk_mgmt_timesheet/models/__init__.py
@@ -0,0 +1,6 @@
+###############################################################################
+# For copyright and license notices, see __manifest__.py file in root directory
+###############################################################################
+from . import hr_timesheet
+from . import helpdesk_ticket
+from . import helpdesk_ticket_team
