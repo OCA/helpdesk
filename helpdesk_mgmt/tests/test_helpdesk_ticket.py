761d5b0a0b54b5a25628dfe520366513ee8ed9e1
eLBati <lb@takobi.online>
2020-07-30 12:11:00 +0200

    [IMP] helpdesk_mgmt: Test access added
    Forward port of dfbc3698e76b3a1330f339ca78b49557b78c05ed

--- helpdesk_mgmt/tests/test_helpdesk_ticket.py
+++ helpdesk_mgmt/tests/test_helpdesk_ticket.py
@@ -1,5 +1,7 @@
 import time
 
+from odoo.tests import tagged
+
 from .common import TestHelpdeskTicketBase
 
 
@@ -181,3 +183,101 @@ class TestHelpdeskTicket(TestHelpdeskTicketBase):
             }
         )
         self.assertEqual(self.new_stage, new_ticket.stage_id)
+
+    def test_helpdesk_ticket_multiple(self):
+        Ticket = self.env["helpdesk.ticket"]
+        Partner = self.env["res.partner"]
+
+        partner_1 = Partner.create({"name": "Partner 1", "email": "partner1@test.com"})
+        partner_2 = Partner.create({"name": "Partner 2", "email": "partner2@test.com"})
+        tickets = Ticket.create(
+            [
+                {
+                    "name": "Test 1",
+                    "description": "Ticket test number 1",
+                    "user_id": self.user.id,
+                    "partner_email": "partner@domain.com",
+                },
+                {
+                    "name": "Test 2",
+                    "description": "Ticket test number 2",
+                    "user_id": self.user.id,
+                    "partner_id": partner_1.id,
+                },
+                {
+                    "name": "Test 3",
+                    "description": "Ticket test number 3",
+                    "user_id": self.user_demo.id,
+                    "partner_id": partner_2.id,
+                },
+            ]
+        )
+        self.assertEqual(len(tickets), 3)
+
+    def test_helpdesk_ticket_access(self):
+        Ticket = self.env["helpdesk.ticket"]
+        t = Ticket.create(
+            {
+                "name": "Test 1",
+                "description": "Ticket test",
+            }
+        )
+        self.assertEqual(t.access_url, "/my/ticket/%s" % t.id)
+        t.partner_id = self.user_portal.partner_id.id
+        self.assertTrue(t.partner_can_access())
+
+    def test_partner_can_access(self):
+        Ticket = self.env["helpdesk.ticket"]
+        self.assertFalse(Ticket.partner_can_access())
+        p = self.env.ref("base.res_partner_1")
+        t = Ticket.create(
+            {
+                "name": "Test 1",
+                "description": "Ticket test",
+                "partner_id": p.id,
+            }
+        )
+        self.assertFalse(t.partner_can_access())
+        t.partner_id = self.env.ref("base.user_admin").partner_id.id
+        self.assertTrue(t.partner_can_access())
+
+    def test_access_url(self):
+        Ticket = self.env["helpdesk.ticket"]
+        t = Ticket.create(
+            {
+                "name": "Test 1",
+                "description": "Ticket test",
+            }
+        )
+        t._compute_access_url()
+        self.assertEqual(t.access_url, "/my/ticket/%s" % t.id)
+
+
+@tagged("post_install", "-at_install")
+class TestHelpdeskTicketPostInstall(TestHelpdeskTicketBase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+
+    def test_helpdesk_ticket_assign_notify(self):
+        Ticket = self.env["helpdesk.ticket"]
+        Message = self.env["mail.message"]
+
+        t = Ticket.create(
+            {
+                "name": "Test 1",
+                "description": "Ticket test",
+            }
+        )
+        t.user_id = self.user_demo.id
+        m = Message.search(
+            [
+                ("model", "=", "helpdesk.ticket"),
+                ("res_id", "=", t.id),
+                ("message_type", "=", "user_notification"),
+            ]
+        )
+        self.assertTrue(
+            "You have been assigned to the Helpdesk Ticket %s" % t.display_name
+            in m.body
+        )
