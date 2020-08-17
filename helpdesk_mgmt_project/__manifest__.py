f697cbe09df422676fa786faf077cf60f734a59f
David Alonso (Solvos) <david.alonso@solvos.es>
2020-08-17 17:19:33 +0200

    [IMP] helpdesk_mgmt_project: black, isort, prettier

--- helpdesk_mgmt_project/__manifest__.py
+++ helpdesk_mgmt_project/__manifest__.py
@@ -1,23 +1,19 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'Helpdesk Project',
-    'summary': "Add the option to select project in the tickets.",
-    'version': '12.0.1.1.0',
-    'license': 'AGPL-3',
-    'category': 'After-Sales',
-    'author': 'PuntSistemes S.L.U., '
-              'Odoo Community Association (OCA)',
-    'website': 'https://github.com/OCA/helpdesk',
-    'depends': [
-        'helpdesk_mgmt',
-        'project',
+    "name": "Helpdesk Project",
+    "summary": "Add the option to select project in the tickets.",
+    "version": "12.0.1.1.0",
+    "license": "AGPL-3",
+    "category": "After-Sales",
+    "author": "PuntSistemes S.L.U., " "Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/helpdesk",
+    "depends": ["helpdesk_mgmt", "project",],
+    "data": [
+        "views/helpdesk_ticket_view.xml",
+        "views/project_view.xml",
+        "views/project_task_view.xml",
     ],
-    'data': [
-        'views/helpdesk_ticket_view.xml',
-        'views/project_view.xml',
-        'views/project_task_view.xml',
-    ],
-    'development_status': 'Beta',
-    'auto_install': True,
+    "development_status": "Beta",
+    "auto_install": True,
 }
