70140a8da627e80111fe234cbd4e8bfcd32757fc
David Alonso (Solvos) <david.alonso@solvos.es>
2020-08-17 17:19:33 +0200

    [IMP] helpdesk_mgmt_project: black, isort, prettier

--- helpdesk_mgmt_project/models/helpdesk_ticket.py
+++ helpdesk_mgmt_project/models/helpdesk_ticket.py
@@ -1,19 +1,13 @@
-from odoo import fields, models, api
+from odoo import api, fields, models
 
 
 class HelpdeskTicket(models.Model):
 
     _inherit = "helpdesk.ticket"
 
-    project_id = fields.Many2one(
-        string='Project',
-        comodel_name='project.project'
-    )
-    task_id = fields.Many2one(
-        string='Task',
-        comodel_name='project.task'
-    )
+    project_id = fields.Many2one(string="Project", comodel_name="project.project")
+    task_id = fields.Many2one(string="Task", comodel_name="project.task")
 
-    @api.onchange('project_id')
+    @api.onchange("project_id")
     def _onchange_project(self):
         self.task_id = False
