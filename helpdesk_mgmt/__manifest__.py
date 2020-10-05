4f5be853b68496332e7586a2d9e61d69308fd750
Yann Papouin <y.papouin@dec-industrie.com>
2020-10-05 13:07:29 +0200

    [FIX] helpdesk_mgmt: Allow base user to access partner data without being in a Helpdesk group

--- helpdesk_mgmt/__manifest__.py
+++ helpdesk_mgmt/__manifest__.py
@@ -4,7 +4,7 @@
     "name": "Helpdesk Management",
     "summary": """
         Helpdesk""",
-    "version": "14.0.1.2.0",
+    "version": "14.0.1.3.0",
     "license": "AGPL-3",
     "category": "After-Sales",
     "author": "AdaptiveCity, "
