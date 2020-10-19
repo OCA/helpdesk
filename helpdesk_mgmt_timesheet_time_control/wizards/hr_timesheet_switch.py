89faa41c9b7cc2e8b4a22f08727e11a3ed9626b6
Graeme Gellatly <gdgellatly@gmail.com>
2020-10-20 10:58:56 +1300

    [NEW] Helpdesk_mgmt_timesheet_time_control
    This new module extends the integration between helpdesk and timeheets. If project_timeshet_time_control and helpdesk_mgmt_timesheet are installed this module will autoinstall.
    
    Fixups from review Co-authored-by: Jairo Llopis <Yajo@users.noreply.github.com>

---
+++ helpdesk_mgmt_timesheet_time_control/wizards/hr_timesheet_switch.py
@@ -0,0 +1,27 @@
+# Copyright 2020 Graeme Gellatly
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import api, models
+
+
+class HrTimesheetSwitch(models.TransientModel):
+    _inherit = "hr.timesheet.switch"
+
+    @api.model
+    def _closest_suggestion(self):
+        """Allow searching best suggestion by helpdesk.ticket."""
+        result = super()._closest_suggestion()
+        try:
+            if not result and self.env.context["active_model"] == "helpdesk.ticket":
+                return self.env["account.analytic.line"].search(
+                    [
+                        ("user_id", "=", self.env.user.id),
+                        ("ticket_id", "=", self.env.context["active_id"]),
+                    ],
+                    order="date_time DESC",
+                    limit=1,
+                )
+        except KeyError:
+            # If I don't know where's the user, I don't know what to suggest
+            pass
+        return result
