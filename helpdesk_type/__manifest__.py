4a52a5d9a5331d772209507a52988ff9566c9a5b
Darío Lodeiros <dario@commitsun.com>
2020-11-17 09:39:31 +0100

    [FIX] Development Status
    Maintainer Task

--- helpdesk_type/__manifest__.py
+++ helpdesk_type/__manifest__.py
@@ -21,7 +21,7 @@
         "views/helpdesk_ticket.xml",
     ],
     "application": False,
-    "development_status": "Stable",
+    "development_status": "Beta",
     "maintainers": [
         "nelsonramirezs",
         "max3903",
