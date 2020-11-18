75da0dbeb181b9e35a6b845af4634bafc7c52fb9
David Alonso <david.alonso@solvos.es>
2020-11-18 18:27:24 +0100

    [MIG] helpdesk_mgmt: Migration to 14.0

--- helpdesk_mgmt/__manifest__.py
+++ helpdesk_mgmt/__manifest__.py
@@ -4,7 +4,7 @@
     "name": "Helpdesk Management",
     "summary": """
         Helpdesk""",
-    "version": "13.0.1.1.0",
+    "version": "14.0.1.0.0",
     "license": "AGPL-3",
     "category": "After-Sales",
     "author": "AdaptiveCity, "
