135c144a69fef271170b200784efb96031e180ee
David Alonso // Solvos <david.alonso@solvos.es>
2021-02-11 11:45:37 +0100

    [IMP] helpdesk_mgmt_timesheet: filter tickets with activity on timesheets today

--- helpdesk_mgmt_timesheet/tests/test_helpdesk_mgmt_timesheet.py
+++ helpdesk_mgmt_timesheet/tests/test_helpdesk_mgmt_timesheet.py
@@ -2,6 +2,7 @@
 # For copyright and license notices, see __manifest__.py file in root directory
 ###############################################################################
 import logging
+from datetime import timedelta
 
 from odoo import fields
 
@@ -23,11 +24,11 @@ class TestHelpdeskMgmtTimesheet(test_helpdesk_ticket.TestHelpdeskTicket):
             }
         )
 
-    def generate_timesheet(self, ticket, hours=1.0):
+    def generate_timesheet(self, ticket, hours=1.0, days_ago=0):
         return self.env["account.analytic.line"].create(
             {
                 "amount": 0,
-                "date": fields.Date.today(),
+                "date": fields.Date.today() - timedelta(days=days_ago),
                 "name": "Test Timesheet",
                 "unit_amount": hours,
                 "ticket_id": ticket.id,
@@ -46,11 +47,18 @@ class TestHelpdeskMgmtTimesheet(test_helpdesk_ticket.TestHelpdeskTicket):
 
     def test_helpdesk_mgmt_timesheet(self):
         ticket = self.generate_ticket()
+        self.assertFalse(ticket.last_timesheet_activity)
         ticket._onchange_team_id()
         self.assertEqual(ticket.project_id.id, self.team_id.default_project_id.id)
         ticket.planned_hours = 5
-        timesheet1 = self.generate_timesheet(ticket, 2)
-        timesheet2 = self.generate_timesheet(ticket, 1)
+        days_ago = 1
+        timesheet1 = self.generate_timesheet(ticket, hours=2, days_ago=days_ago)
+        self.assertEqual(
+            ticket.last_timesheet_activity,
+            fields.Date.today() - timedelta(days=days_ago),
+        )
+        timesheet2 = self.generate_timesheet(ticket, hours=1)
+        self.assertEqual(ticket.last_timesheet_activity, fields.Date.today())
         self.assertEqual(
             ticket.total_hours, timesheet1.unit_amount + timesheet2.unit_amount
         )
