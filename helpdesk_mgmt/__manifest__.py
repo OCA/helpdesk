67b849248f1040878a989f0ad6de3452428f211b
Simone Orsi <simahawk@users.noreply.github.com>
2021-05-26 08:15:46 +0200

    Update __manifest__.py

--- helpdesk_mgmt/__manifest__.py
+++ helpdesk_mgmt/__manifest__.py
@@ -1,7 +1,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    "name": "Helpdesk Management",
+    "name": "Helpdesk Management TEST DO NOT MERGE ME",
     "summary": """
         Helpdesk""",
     "version": "14.0.1.0.0",
