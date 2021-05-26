f7defd8cea893a24a8f9f90288dbe5cf29bc0a5a
David Alonso // Solvos <david.alonso@solvos.es>
2021-05-26 10:45:08 +0200

    [MIG] helpdesk_motive: Migration to 14.0

--- helpdesk_motive/models/__init__.py
+++ helpdesk_motive/models/__init__.py
@@ -2,4 +2,5 @@
 # Copyright (C) 2019 Konos
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 from . import helpdesk_ticket
+from . import helpdesk_ticket_team
 from . import helpdesk_ticket_motive
