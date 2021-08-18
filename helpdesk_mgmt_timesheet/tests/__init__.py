ee07f108f42dec1b974c29962589930af45f6dec
aromera <aromera@pmsastro.es>
2021-08-18 15:27:42 +0200

    [IMP] Merge helpdesk_mgmt_timeheet_time_control

--- helpdesk_mgmt_timesheet/tests/__init__.py
+++ helpdesk_mgmt_timesheet/tests/__init__.py
@@ -2,3 +2,4 @@
 # For copyright and license notices, see __manifest__.py file in root directory
 ###############################################################################
 from . import test_helpdesk_mgmt_timesheet
+from . import test_helpdesk_timesheet_time_control
