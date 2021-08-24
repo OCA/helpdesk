6ac7c814e9fca39e5ef6f09d7b682f9859196436
Héctor Garrido Colmenarejo <hector.garrido@garcoconsulting.es>
2021-08-24 08:43:41 +0200

    [SQUASH] 13.0-helpdesk_mgmt_sla

--- helpdesk_mgmt_sla/tests/__init__.py
+++ helpdesk_mgmt_sla/tests/__init__.py
@@ -1,6 +1,4 @@
 #    Copyright (C) 2020 GARCO Consulting <www.garcoconsulting.es>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
-from . import helpdesk_sla
-from . import helpdesk_ticket
-from . import helpdesk_ticket_team
+from . import test_helpdesk_mgmt_sla
