1232a533741325559bf65ed2ef49e921446c0d3e
Héctor Garrido Colmenarejo <hector.garrido@garcoconsulting.es>
2021-08-24 08:43:41 +0200

    [SQUASH] 13.0-helpdesk_mgmt_sla

--- helpdesk_mgmt_sla/models/helpdesk_sla.py
+++ helpdesk_mgmt_sla/models/helpdesk_sla.py
@@ -1,4 +1,4 @@
-#    Copyright (C) 2020 GARCO Consulting <www.garcoconsulting.com>
+#    Copyright (C) 2020 GARCO Consulting <www.garcoconsulting.es>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 from datetime import datetime
@@ -11,20 +11,21 @@ class HelpdeskSla(models.Model):
     _description = "Helpdesk SLA"
 
     name = fields.Char(string="Name", required=True)
-    team_id = fields.Many2one("helpdesk.ticket.team", "Team", required=True)
+    team_ids = fields.Many2many(comodel_name="helpdesk.ticket.team", string="Teams")
     stage_id = fields.Many2one(comodel_name="helpdesk.ticket.stage", string="Stage")
     days = fields.Integer(string="Days", default=0, required=True)
     hours = fields.Integer(string="Hours", default=0, required=True)
     note = fields.Char(string="Note")
 
     def check_sla(self):
-        slas = self.search([("team_id", "!=", False)])
+        slas = self.search([("team_ids", "!=", False)])
         for sla in slas:
-            if sla.team_id.ticket_ids:
-                sla.check_ticket_sla()
+            for team in sla.team_ids:
+                if team.ticket_ids:
+                    sla.check_ticket_sla(team.ticket_ids)
 
-    def check_ticket_sla(self):
-        for ticket in self.team_id.ticket_ids:
+    def check_ticket_sla(self, ticket_ids):
+        for ticket in ticket_ids.filtered(lambda t: not t.stage_id.closed):
             deadline = ticket.create_date
             working_calendar = ticket.team_id.resource_calendar_id
 
