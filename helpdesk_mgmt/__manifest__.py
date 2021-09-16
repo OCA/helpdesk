da47f7168bb606c80d9b3ce8d0b69ce0bd6f6263
David Alonso // Solvos <david.alonso@solvos.es>
2021-09-16 17:41:39 +0200

    [FIX] helpdesk_mgmt: rename already existing alias

--- helpdesk_mgmt/__manifest__.py
+++ helpdesk_mgmt/__manifest__.py
@@ -4,7 +4,7 @@
     "name": "Helpdesk Management",
     "summary": """
         Helpdesk""",
-    "version": "14.0.1.1.0",
+    "version": "14.0.1.1.1",
     "license": "AGPL-3",
     "category": "After-Sales",
     "author": "AdaptiveCity, "
