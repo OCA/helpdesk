af0f75128230a2ed6b357e748474d098575cb97a
Jorge Luis Quinteros <quinterosorellana04@gmail.com>
2022-02-28 21:41:39 +0100

    [IMP] Restore the fields in the ticket creation
    The way it worked until the change was that these fields were populated with the customer's information when the ticket was created.
    And to preserve that functionality I think it is necessary to keep informing those fields when creating the ticket from the portal.
    Because when creating the ticket from the back-end the behavior is similar, when selecting a customer, the fields are filled automatically.

--- helpdesk_mgmt/__manifest__.py
+++ helpdesk_mgmt/__manifest__.py
@@ -4,7 +4,7 @@
     "name": "Helpdesk Management",
     "summary": """
         Helpdesk""",
-    "version": "15.0.1.3.1",
+    "version": "15.0.1.3.2",
     "license": "AGPL-3",
     "category": "After-Sales",
     "author": "AdaptiveCity, "
