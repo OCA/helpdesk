27a9b84ef29a7b47e2264a41e18e252b59b2ae87
Miquel Raïch <miquel.raich@forgeflow.com>
2022-03-25 10:49:33 +0100

    [OU-FIX] helpdesk_mgmt: migrate company rules

---
+++ helpdesk_mgmt/migrations/13.0.1.8.1/post-migration.py
@@ -0,0 +1,11 @@
+# Copyright 2022 ForgeFlow S.L. (https://www.forgeflow.com)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from openupgradelib import openupgrade
+
+
+@openupgrade.migrate()
+def migrate(env, version):
+    openupgrade.load_data(
+        env.cr, "helpdesk_mgmt", "migrations/13.0.1.8.1/noupdate_changes.xml"
+    )
