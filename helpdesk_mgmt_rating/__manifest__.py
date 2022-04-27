e12b2519ef0b9a0c8f95a1fc6c46c725662af6a7
david <david.vidal@tecnativa.com>
2022-04-27 18:20:16 +0200

    [IMP] helpdesk_mgmt_rating: full ratings views
    In core we enjoy a wide range of view modes available for rating
    actions.
    
    TT36164
    
    helpdesk_mgmt_rating 14.0.1.1.0
    
    Added translation using Weblate (French)
    
    Translated using Weblate (French)
    
    Currently translated at 100.0% (24 of 24 strings)
    
    Translation: helpdesk-14.0/helpdesk-14.0-helpdesk_mgmt_rating
    Translate-URL: https://translation.odoo-community.org/projects/helpdesk-14-0/helpdesk-14-0-helpdesk_mgmt_rating/fr/

--- helpdesk_mgmt_rating/__manifest__.py
+++ helpdesk_mgmt_rating/__manifest__.py
@@ -4,7 +4,7 @@
         This module allows customer to rate the assistance received
         on a ticket.
         """,
-    "version": "14.0.1.0.0",
+    "version": "14.0.1.1.0",
     "license": "AGPL-3",
     "author": "Domatix, " "Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/helpdesk",
