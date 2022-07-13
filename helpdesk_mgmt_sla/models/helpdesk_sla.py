adf0ef768bd2c713c799c181042fd7d0657b96bc
Marcel Savegnago <marcel.savegnago@gmail.com>
2022-07-13 10:30:22 -0300

    [IMP] helpdesk_mgmt_sla: add freezed stages and freezed hours

--- helpdesk_mgmt_sla/models/helpdesk_sla.py
+++ helpdesk_mgmt_sla/models/helpdesk_sla.py
@@ -16,6 +16,13 @@ class HelpdeskSla(models.Model):
     days = fields.Integer(string="Days", default=0, required=True)
     hours = fields.Integer(string="Hours", default=0, required=True)
     note = fields.Char(string="Note")
+    freeze_stage_ids = fields.Many2many(
+        "helpdesk.ticket.stage",
+        string="Freezing Stages",
+        domain="[('id', '!=', stage_id.id)]",
+        help="The amount of time the ticket spends in this stage will be added "
+        "to the SLA Policy deadline.",
+    )
 
     def check_sla(self):
         slas = self.search([("team_ids", "!=", False)])
@@ -24,10 +31,41 @@ class HelpdeskSla(models.Model):
                 if team.ticket_ids:
                     sla.check_ticket_sla(team.ticket_ids)
 
+    def compute_freezed_hours(self, working_calendar, ticket):
+        self.ensure_one()
+        hours_freezed = 0
+
+        field_stage = self.env["ir.model.fields"]._get(ticket._name, "stage_id")
+        freeze_stages = self.freeze_stage_ids.ids
+
+        tracking_lines = ticket.message_ids.tracking_value_ids.filtered(
+            lambda tv: tv.field == field_stage
+        ).sorted(key="create_date")
+
+        if not tracking_lines:
+            return 0
+
+        old_time = ticket.create_date
+
+        for tracking_line in tracking_lines:
+            if tracking_line.old_value_integer in freeze_stages:
+                hours_freezed += working_calendar.get_work_hours_count(
+                    old_time, tracking_line.create_date
+                )
+            old_time = tracking_line.create_date
+
+        if tracking_lines[-1].new_value_integer in freeze_stages:
+            hours_freezed += working_calendar.get_work_hours_count(
+                old_time, fields.Datetime.now()
+            )
+
+        return hours_freezed
+
     def check_ticket_sla(self, ticket_ids):
         for ticket in ticket_ids.filtered(lambda t: not t.stage_id.closed):
             deadline = ticket.create_date
             working_calendar = ticket.team_id.resource_calendar_id
+            hours = self.hours
 
             if self.days > 0:
                 deadline = working_calendar.plan_days(
@@ -52,10 +90,12 @@ class HelpdeskSla(models.Model):
                         hour=0, minute=0, second=0, microsecond=0
                     )
 
-            deadline = working_calendar.plan_hours(
-                self.hours, deadline, compute_leaves=True
-            )
+            if self.freeze_stage_ids:
+                hours += self.compute_freezed_hours(working_calendar, ticket)
+
+            deadline = working_calendar.plan_hours(hours, deadline, compute_leaves=True)
             ticket.sla_deadline = deadline
+
             if ticket.sla_deadline < datetime.today().now():
                 ticket.sla_expired = True
             else:
