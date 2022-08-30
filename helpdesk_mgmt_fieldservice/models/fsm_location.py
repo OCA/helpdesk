2d9ca2dbdbad40546f7e855ad0b6840d7f42e0a8
Víctor Martínez <victor.martinez@tecnativa.com>
2022-08-30 13:09:07 +0200

    [DCK] helpdesk_mgmt_fieldservice: Change the license from LGPL to AGPL.

--- helpdesk_mgmt_fieldservice/models/fsm_location.py
+++ helpdesk_mgmt_fieldservice/models/fsm_location.py
@@ -1,6 +1,6 @@
 # Copyright (C) 2019 - TODAY, Open Source Integrators
 # Copyright 2020 - TODAY, Marcel Savegnago - Escodoo
-# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from odoo import fields, models
 
--- helpdesk_mgmt_fieldservice/wizards/__init__.py
+++ helpdesk_mgmt_fieldservice/wizards/__init__.py
@@ -1,4 +1,4 @@
 # Copyright (C) 2019 Pavlov Media
-# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from . import fsm_order_close_wizard
