cc2d281262f60ff8d315e705b9db8de554d2d128
bojan <bojanancev@gmail.com>
2022-09-07 15:54:00 +0200

    [ADD] helpdesk_type_sla: implement rules based on type

---
+++ helpdesk_type_sla/__manifest__.py
@@ -0,0 +1,14 @@
+# Copyright 2022 Camptocamp SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Helpdesk Type SLA",
+    "summary": "Helpdesk Type SLA",
+    "version": "14.0.1.0.0",
+    "author": "Camptocamp, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "category": "HelpDesk Service",
+    "depends": ["helpdesk_mgmt_sla", "helpdesk_type"],
+    "website": "https://github.com/OCA/helpdesk",
+    "data": ["views/helpdesk_sla_views.xml"],
+    "installable": True,
+}
