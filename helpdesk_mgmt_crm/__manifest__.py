880694b3238fcc40e7e5aa926ba4b146e2dffbe8
Víctor Martínez <victor.martinez@tecnativa.com>
2022-10-06 12:42:44 +0200

    [FIX] helpdesk_mgmt_crm: Prevent null value in column "notification_type" if message have notifications (not copied)
    TT38675
    
    helpdesk_mgmt_crm 13.0.1.0.1

--- helpdesk_mgmt_crm/__manifest__.py
+++ helpdesk_mgmt_crm/__manifest__.py
@@ -3,7 +3,7 @@
 {
     "name": "Link between Helpdesk and CRM",
     "summary": "Links helpdesk tickets with leads",
-    "version": "13.0.1.0.0",
+    "version": "13.0.1.0.1",
     "category": "After-Sales",
     "website": "https://github.com/OCA/helpdesk",
     "author": "Tecnativa, Odoo Community Association (OCA)",
