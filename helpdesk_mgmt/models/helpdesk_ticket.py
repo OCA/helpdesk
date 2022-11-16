8f03c0dd127e862f8c66f3cc9227b033ee94e4e5
MarcBForgeFlow <marc.belmonte@forgeflow.com>
2022-11-16 15:39:22 +0100

    [FIX] helpdesk_mgmt: sets the reply email in helpdesk team if any

--- helpdesk_mgmt/models/helpdesk_ticket.py
+++ helpdesk_mgmt/models/helpdesk_ticket.py
@@ -251,3 +251,24 @@ class HelpdeskTicket(models.Model):
             # imply modifying followers
             return recipients
         return recipients
+
+    def _notify_get_reply_to(
+        self, default=None, records=None, company=None, doc_names=None
+    ):
+        """Override to set alias of tasks to their team if any."""
+        aliases = (
+            self.sudo()
+            .mapped("team_id")
+            ._notify_get_reply_to(
+                default=default, records=None, company=company, doc_names=None
+            )
+        )
+        res = {ticket.id: aliases.get(ticket.team_id.id) for ticket in self}
+        leftover = self.filtered(lambda rec: not rec.team_id)
+        if leftover:
+            res.update(
+                super(HelpdeskTicket, leftover)._notify_get_reply_to(
+                    default=default, records=None, company=company, doc_names=doc_names
+                )
+            )
+        return res
