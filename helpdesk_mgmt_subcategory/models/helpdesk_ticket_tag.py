086f3e2ba75dc933d203cb22b8e6d66e4551abec
Rafa Morant <rmorant@albasoft.com>
2022-11-30 19:24:05 +0100

    [FIX] from tags, filter open tickets only

--- helpdesk_mgmt_subcategory/models/helpdesk_ticket_tag.py
+++ helpdesk_mgmt_subcategory/models/helpdesk_ticket_tag.py
@@ -4,17 +4,28 @@ from odoo import fields, models
 class HelpdeskTicketTag(models.Model):
     _inherit = "helpdesk.ticket.tag"
 
-    tickets_count = fields.Integer(string="# Tickets", compute="_compute_tickets_count")
+    tickets_count = fields.Integer(
+        string="# Open Tickets", compute="_compute_tickets_count"
+    )
+
+    _compute_tickets_count_sql_all = """SELECT helpdesk_ticket_tag_id , COUNT(*)
+        FROM helpdesk_ticket_helpdesk_ticket_tag_rel
+        WHERE helpdesk_ticket_tag_id in %s
+        GROUP BY helpdesk_ticket_tag_id
+        """
+    _compute_tickets_count_sql_open = """SELECT helpdesk_ticket_tag_id , COUNT(*)
+    FROM helpdesk_ticket_helpdesk_ticket_tag_rel t2tt
+    LEFT JOIN helpdesk_ticket t ON t.id = t2tt.helpdesk_ticket_id
+    WHERE t2tt.helpdesk_ticket_tag_id in %s
+    and t.stage_id not in (select id from helpdesk_ticket_stage where closed=true)
+    GROUP BY helpdesk_ticket_tag_id
+    """
 
     def _compute_tickets_count(self):
         tag_tickets_count = {}
         if self.ids:
             self.env.cr.execute(
-                """SELECT helpdesk_ticket_tag_id , COUNT(*)
-                FROM helpdesk_ticket_helpdesk_ticket_tag_rel
-                WHERE helpdesk_ticket_tag_id in %s
-                GROUP BY helpdesk_ticket_tag_id
-                """,
+                self._compute_tickets_count_sql_open,
                 (tuple(self.ids),),
             )
             tag_tickets_count = dict(self.env.cr.fetchall())
