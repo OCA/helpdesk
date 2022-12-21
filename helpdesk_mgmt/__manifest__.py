831e135f89ffeb4aa4c6a89ec864041ffeae1c32
Víctor Martínez <victor.martinez@tecnativa.com>
2022-12-21 13:35:53 +0100

    [IMP] helpdesk_mgmt: Add team to new ticket form portal page
    TT40877

--- helpdesk_mgmt/__manifest__.py
+++ helpdesk_mgmt/__manifest__.py
@@ -20,6 +20,7 @@
         "security/helpdesk_security.xml",
         "security/ir.model.access.csv",
         "views/res_partner_views.xml",
+        "views/res_config_settings_views.xml",
         "views/helpdesk_ticket_templates.xml",
         "views/helpdesk_ticket_menu.xml",
         "views/helpdesk_ticket_team_views.xml",
