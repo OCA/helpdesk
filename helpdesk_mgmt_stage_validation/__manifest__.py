d554fc3c0a655470a836a213549b48a53d1efac6
BojanOD <bojanancev@gmail.com>
2022-12-22 15:21:35 +0100

    [14.0][ADD] Helpdesk Stage Validation

---
+++ helpdesk_mgmt_stage_validation/__manifest__.py
@@ -0,0 +1,13 @@
+# Copyright 2022 Camptocamp SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Helpdesk Ticket Stage Validation",
+    "summary": "Validate input data when reaching a Helpdesk Ticket stage",
+    "version": "14.0.1.0.0",
+    "category": "After-Sales",
+    "author": "Camptocamp, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/helpdesk",
+    "depends": ["helpdesk_mgmt"],
+    "data": ["views/helpdesk_ticket_stage.xml"],
+    "license": "AGPL-3",
+}
