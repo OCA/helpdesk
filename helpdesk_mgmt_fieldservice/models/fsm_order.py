0172dd61c629de63ca9e5edc5be4c690a106459a
Alessandro Uffreduzzi <alessandro.uffreduzzi@pytech.it>
2023-01-02 15:08:04 +0100

    [14.0][IMP]helpdesk_mgmt_fieldservice: review fsm_order_close_wizard security
    This commit reviews the security rules around the fsm_order_close_wizard.
    
    The "Complete" button on the fsm.order triggered an access error for fieldservice.group_fsm_user_own. The commit allows fieldservice.group_fsm_user_own to use the wizard as well. However, the wizard is now only shown if the user also has write permission on the specific ticket (so as to play nice with a variety of setups, including helpdesk_mgmt.group_helpdesk_user_own).
    
    If the user has permission to write on the ticket, the wizard will be shown as before; otherwise it will simply be skipped, but the fsm.order will be closed as it should.
    
    There's also some code cleanup on the wizard, such as removing the unused team_id field.

--- helpdesk_mgmt_fieldservice/models/fsm_order.py
+++ helpdesk_mgmt_fieldservice/models/fsm_order.py
@@ -3,6 +3,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from odoo import fields, models
+from odoo.exceptions import AccessError
 
 
 class FSMOrder(models.Model):
@@ -13,35 +14,35 @@ class FSMOrder(models.Model):
     def action_complete(self):
         res = super().action_complete()
         if self.ticket_id:
-            open_fsm_orders_count = self.env["fsm.order"].search_count(
-                [
-                    ("ticket_id", "=", self.ticket_id.id),
-                    ("stage_id.is_closed", "=", False),
-                ]
-            )
-
-            if self.ticket_id.stage_id.closed:
-                return res
-            elif open_fsm_orders_count == 0:
-                view_id = self.env.ref(
-                    "helpdesk_mgmt_fieldservice.fsm_order_close_wizard_view_form"
-                ).id
-                return {
-                    "view_id": view_id,
-                    "view_mode": "form",
-                    "res_model": "fsm.order.close.wizard",
-                    "type": "ir.actions.act_window",
-                    "target": "new",
-                    "context": {
-                        "default_ticket_id": self.ticket_id.id,
-                        "default_team_id": self.ticket_id.team_id.id,
-                        "default_resolution": self.resolution,
-                    },
-                }
+            try:
+                self.ticket_id.check_access_rights("write")
+                self.ticket_id.check_access_rule("write")
+            except AccessError:
+                pass
             else:
-                return res
-        else:
-            return res
+                if not self.ticket_id.stage_id.closed:
+                    open_fsm_orders_count = self.env["fsm.order"].search_count(
+                        [
+                            ("ticket_id", "=", self.ticket_id.id),
+                            ("stage_id.is_closed", "=", False),
+                        ]
+                    )
+                    if open_fsm_orders_count == 0:
+                        view_id = self.env.ref(
+                            "helpdesk_mgmt_fieldservice.fsm_order_close_wizard_view_form"
+                        ).id
+                        return {
+                            "view_id": view_id,
+                            "view_mode": "form",
+                            "res_model": "fsm.order.close.wizard",
+                            "type": "ir.actions.act_window",
+                            "target": "new",
+                            "context": {
+                                "default_ticket_id": self.ticket_id.id,
+                                "default_resolution": self.resolution,
+                            },
+                        }
+        return res
 
     def action_view_order(self):
         """
