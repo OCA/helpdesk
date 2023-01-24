98150bac54d93709b258bfd647b90ebc16b079ee
Víctor Martínez <victor.martinez@tecnativa.com>
2023-01-24 10:51:44 +0100

    [IMP] helpdesk_mgmt: Fix Internal Tickets rule to prevent see other users' tickets + improve tests
    TT41277

---
+++ helpdesk_mgmt/migrations/13.0.2.5.0/post-migration.py
@@ -0,0 +1,11 @@
+# Copyright 2023 Tecnativa - Víctor Martínez
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from openupgradelib import openupgrade
+
+
+@openupgrade.migrate()
+def migrate(env, version):
+    openupgrade.load_data(
+        env.cr, "helpdesk_mgmt", "migrations/13.0.2.5.0/noupdate_changes.xml"
+    )
