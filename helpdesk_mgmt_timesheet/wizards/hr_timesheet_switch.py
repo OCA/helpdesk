4c738e04b0013fd086ce513e422d215effa5e65e
Aromera <aromera@pmsastro.es>
2023-04-18 10:43:10 +0200

    [MIG] helpdesk_mgmt_timesheet: Migration to 16.0

--- helpdesk_mgmt_timesheet/wizards/hr_timesheet_switch.py
+++ helpdesk_mgmt_timesheet/wizards/hr_timesheet_switch.py
@@ -1,7 +1,11 @@
 # Copyright 2020 Graeme Gellatly
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
+import logging
+
 from odoo import api, models
+
+_logger = logging.getLogger(__name__)
 
 
 class HrTimesheetSwitch(models.TransientModel):
@@ -22,6 +26,5 @@ class HrTimesheetSwitch(models.TransientModel):
                     limit=1,
                 )
         except KeyError:
-            # If I don't know where's the user, I don't know what to suggest
-            pass
+            _logger.warning("Undefinde user in self.env")
         return result
