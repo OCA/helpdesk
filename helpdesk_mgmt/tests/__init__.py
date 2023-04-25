d3cbf2978bd876e41ccc3d7ee4a52b5d5dc8e0b7
Yann Papouin <ypa@decgroupe.com>
2023-04-25 17:26:33 +0200

    [IMP] helpdesk_mgmt: Test ticket created after a mail fetch

--- helpdesk_mgmt/tests/__init__.py
+++ helpdesk_mgmt/tests/__init__.py
@@ -1,4 +1,5 @@
 from . import test_helpdesk_ticket
 from . import test_helpdesk_ticket_team
 from . import test_helpdesk_portal
+from . import test_helpdesk_fetchmail
 from . import test_res_partner
