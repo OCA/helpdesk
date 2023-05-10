2bb85bc54eb2f38d805c3e933294b640402d931c
Yann Papouin <ypa@decgroupe.com>
2023-05-10 14:51:49 +0200

    [FIX] helpdesk_mgmt_timesheet: Ticket submit broken
    If `helpdesk_mgmt_project` is installed without
    `helpdesk_mgmt_timesheet`, an `AttributeError` is raised when submitting
    a new ticket

---
+++ helpdesk_mgmt_timesheet/controllers/main.py
@@ -0,0 +1,19 @@
+# Copyright 2023 ForgeFlow S.L. (https://www.forgeflow.com)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+import odoo.http as http
+
+from odoo.addons.helpdesk_mgmt.controllers.main import HelpdeskTicketController
+
+
+class CustomHelpdeskTicketController(HelpdeskTicketController):
+    def _prepare_submit_ticket_vals(self, **kw):
+        vals = super(CustomHelpdeskTicketController, self)._prepare_submit_ticket_vals(
+            **kw
+        )
+        team = (
+            http.request.env["helpdesk.ticket.team"].sudo().browse(vals.get("team_id"))
+        )
+        if team.default_project_id and not vals.get("project_id"):
+            vals["project_id"] = team.default_project_id.id
+        return vals
