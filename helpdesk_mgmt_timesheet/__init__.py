6eca801f11e3a03aae2bc8492449652f1a872725
Yann Papouin <ypa@decgroupe.com>
2023-05-10 14:51:49 +0200

    [FIX] helpdesk_mgmt_timesheet: Ticket submit broken
    If `helpdesk_mgmt_project` is installed without
    `helpdesk_mgmt_timesheet`, an `AttributeError` is raised when submitting
    a new ticket

--- helpdesk_mgmt_timesheet/__init__.py
+++ helpdesk_mgmt_timesheet/__init__.py
@@ -2,3 +2,4 @@
 # For copyright and license notices, see __manifest__.py file in root directory
 ###############################################################################
 from . import models
+from . import controllers
