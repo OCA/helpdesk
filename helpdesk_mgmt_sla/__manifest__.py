20c1b4f5f583cd53c5eb0382cce71d1caafd81e2
Albert Mendez <j.mendez@binhex.es>
2023-07-21 09:27:00 +0000

    [15.0][MIG] helpdesk_mgmt_sla: Migration to 15.0

--- helpdesk_mgmt_sla/__manifest__.py
+++ helpdesk_mgmt_sla/__manifest__.py
@@ -7,7 +7,7 @@
     "website": "https://github.com/OCA/helpdesk",
     "license": "AGPL-3",
     "category": "After-Sales",
-    "version": "14.0.2.0.0",
+    "version": "15.0.1.0.0",
     "depends": ["base", "helpdesk_mgmt", "resource"],
     "data": [
         "data/helpdesk_sla_cron.xml",
--- helpdesk_mgmt_sla/models/helpdesk_sla.py
+++ helpdesk_mgmt_sla/models/helpdesk_sla.py
@@ -12,17 +12,17 @@ class HelpdeskSla(models.Model):
     _inherit = ["mail.thread", "mail.activity.mixin"]
     _description = "Helpdesk SLA"
 
-    name = fields.Char(string="Name", required=True)
+    name = fields.Char(required=True)
     team_ids = fields.Many2many(comodel_name="helpdesk.ticket.team", string="Teams")
     category_ids = fields.Many2many(
         comodel_name="helpdesk.ticket.category", string="Categories"
     )
     tag_ids = fields.Many2many(comodel_name="helpdesk.ticket.tag", string="Tags")
     stage_ids = fields.Many2many(comodel_name="helpdesk.ticket.stage", string="Stages")
-    days = fields.Integer(string="Days", default=0, required=True)
+    days = fields.Integer(default=0, required=True)
     hours = fields.Integer(string="Hours", default=0, required=True)
-    note = fields.Html(string="Note")
-    domain = fields.Char(string="Filter", default="[]")
+    note = fields.Html()
+    domain = fields.Char(default="[]")
     active = fields.Boolean(default=True)
 
     def _applies_for(self, ticket):
