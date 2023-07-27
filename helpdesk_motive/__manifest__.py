65bb921b3ade27d2c61d2e714b914bf421786516
Adasat <a.torres@binhex.es>
2023-07-27 14:11:02 +0000

    [15.0][MIG]helpdesk_motive : Migration to 15.0

--- helpdesk_motive/__manifest__.py
+++ helpdesk_motive/__manifest__.py
@@ -1,13 +1,14 @@
 # Copyright (C) 2019 Open Source Integrators
 # Copyright (C) 2019 Konos
+# Copyright (C) 2023 Binhex - Adasat Torres
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     "name": "Helpdesk Motive",
-    "version": "14.0.1.0.2",
+    "version": "15.0.1.0.0",
     "license": "AGPL-3",
     "summary": "Keep the motive ",
-    "author": "Konos, Open Source Integrators, Odoo Community Association (OCA)",
+    "author": "Binhex ,Konos, Open Source Integrators, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/helpdesk",
     "depends": ["helpdesk_mgmt"],
     "data": [
