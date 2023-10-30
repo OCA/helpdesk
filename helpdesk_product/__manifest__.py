ad10a10f60239e55df56264d40fc82307e714bb4
MariaBForgeFlow <maria.buscalla@forgeflow.com>
2023-10-30 11:55:52 +0100

    [ADD] helpdesk_product

---
+++ helpdesk_product/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    "name": "Helpdesk Product",
+    "summary": "Add the option to select product in the tickets.",
+    "author": "ForgeFlow, " "Odoo Community Association (OCA)",
+    "version": "16.0.1.0.0",
+    "license": "AGPL-3",
+    "category": "After-Sales",
+    "website": "https://github.com/OCA/helpdesk",
+    "depends": ["helpdesk_mgmt", "sale_management"],
+    "data": ["views/helpdesk_ticket_view.xml"],
+    "development_status": "Beta",
+    "auto_install": True,
+}
