84353f724df47ff58afc22e64bf71722156f6679
Anjeel Haria <bytemeasap@gmail.com>
2024-04-04 11:22:28 +0530

    [FIX] helpdesk_mgmt: use company of team on tickets when team is defined

--- helpdesk_mgmt/models/helpdesk_ticket.py
+++ helpdesk_mgmt/models/helpdesk_ticket.py
@@ -147,6 +147,12 @@ class HelpdeskTicket(models.Model):
                 vals["number"] = self._prepare_ticket_number(vals)
             if vals.get("user_id") and not vals.get("assigned_date"):
                 vals["assigned_date"] = fields.Datetime.now()
+            if vals.get("team_id"):
+                vals["company_id"] = (
+                    self.env["helpdesk.ticket.team"]
+                    .browse([vals["team_id"]])
+                    .company_id.id
+                )
         return super().create(vals_list)
 
     def copy(self, default=None):
