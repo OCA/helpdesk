c9d815f80bf5d6839516939ad1b2d563554ae3e8
aronabencherif <aronabencherif.diatta@xcg.africa>
2024-04-12 10:18:13 +0000

    [IMP] helpdesk_mgmt_project: Set by default the company as the project one in tickets

--- helpdesk_mgmt_project/models/helpdesk_ticket.py
+++ helpdesk_mgmt_project/models/helpdesk_ticket.py
@@ -5,7 +5,9 @@ class HelpdeskTicket(models.Model):
 
     _inherit = "helpdesk.ticket"
 
-    project_id = fields.Many2one(string="Project", comodel_name="project.project")
+    project_id = fields.Many2one(
+        "project.project", string="Project", tracking=True, check_company=True
+    )
     task_id = fields.Many2one(
         string="Task",
         comodel_name="project.task",
@@ -13,6 +15,34 @@ class HelpdeskTicket(models.Model):
         readonly=False,
         store=True,
     )
+
+    @api.model
+    def _default_company_id(self):
+        if self._context.get("default_project_id"):
+            return (
+                self.env["project.project"]
+                .browse(self._context["default_project_id"])
+                .company_id
+            )
+        return self.env.company
+
+    # Override field
+    company_id = fields.Many2one(
+        "res.company",
+        string="Company",
+        compute="_compute_company_id",
+        store=True,
+        readonly=False,
+        required=True,
+        copy=True,
+        default=_default_company_id,
+    )
+
+    @api.depends("project_id.company_id")
+    def _compute_company_id(self):
+        for ticket in self:
+            if ticket.project_id:
+                ticket.company_id = ticket.project_id.company_id
 
     @api.depends("project_id")
     def _compute_task_id(self):
