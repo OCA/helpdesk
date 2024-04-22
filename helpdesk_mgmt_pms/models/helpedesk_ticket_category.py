de091e475af90d744efe326f64010aa5a5fe7317
irluidev <iramirez.spain@gmail.com>
2024-04-22 12:51:26 +0200

    Modify category

--- helpdesk_mgmt_pms/models/helpedesk_ticket_category.py
+++ helpdesk_mgmt_pms/models/helpedesk_ticket_category.py
@@ -8,9 +8,11 @@ class HelpdeskCategory(models.Model):
 
     _inherit = "helpdesk.ticket.category"
 
+    # Sobrescribe el campo company_id cambiándolo a Many2many
     company_ids = fields.Many2many(
         comodel_name="res.company",
+        relation="helpdesk_ticket_category_company_rel",
+        column1="category_id",
+        column2="company_id",
         string="Company",
-        required=True,
-        default=lambda self: self.env.company,
     )
