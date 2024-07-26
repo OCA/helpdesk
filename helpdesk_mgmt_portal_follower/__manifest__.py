2727723c1c84b724e4c53012f618ee55e609a92a
bobrador <bobrador@apsl.net>
2024-07-26 10:00:41 +0200

    [ADD][16.0] helpdesk_mgmt_portal_follower: New module

---
+++ helpdesk_mgmt_portal_follower/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    "name": "Portal Follower",
+    "summary": "Add ticket followers from website portal",
+    "author": (
+        "Advanced Programming Solutions S.L. - APSL - Nagarro, "
+        "Bernat Obrador, Patryk Pyczko, Miquel Pascual, Odoo Community Association (OCA)"
+    ),
+    "license": "AGPL-3",
+    "mantainers": ["BernatObrador", "ppyczko", "mpascuall"],
+    "website": "https://github.com/OCA/helpdesk",
+    "category": "Helpdesk",
+    "version": "16.0.1.0.0",
+    "depends": ["helpdesk_mgmt"],
+    "data": ["views/helpdesk_templates.xml"],
+    "installable": True,
+}
