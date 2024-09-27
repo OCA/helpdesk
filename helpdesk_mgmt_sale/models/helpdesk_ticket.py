896f5abb6adefa7c5a332632b0010e04d2b52e5d
pilarvargas-tecnativa <pilar.vargas@tecnativa.com>
2024-09-27 16:14:47 +0200

    [IMP] helpdesk_mgmt_sale: Allow to assign multiple tickets to an order
    TT50683
    
    [UPD] Update helpdesk_mgmt_sale.pot
    
    [BOT] post-merge updates
    
    Update translation files
    
    Updated by "Update PO files to match POT (msgmerge)" hook in Weblate.
    
    Translation: helpdesk-15.0/helpdesk-15.0-helpdesk_mgmt_sale
    Translate-URL: https://translation.odoo-community.org/projects/helpdesk-15-0/helpdesk-15-0-helpdesk_mgmt_sale/

--- helpdesk_mgmt_sale/models/helpdesk_ticket.py
+++ helpdesk_mgmt_sale/models/helpdesk_ticket.py
@@ -4,7 +4,7 @@ from odoo import api, fields, models
 class HelpdeskTicket(models.Model):
     _inherit = "helpdesk.ticket"
 
-    sale_order_ids = fields.One2many("sale.order", "ticket_id")
+    sale_order_ids = fields.Many2many("sale.order")
     so_count = fields.Integer(
         string="Sale Order Count", compute="_compute_so_count", store=True
     )
@@ -17,9 +17,9 @@ class HelpdeskTicket(models.Model):
     def action_view_sale_orders(self):
         """Returns action to view sale orders related to this ticket."""
         action = self.env["ir.actions.actions"]._for_xml_id("sale.action_orders")
-        action["domain"] = [("ticket_id", "=", self.id)]
+        action["domain"] = [("ticket_ids", "in", [self.id])]
         action["context"] = {
-            "default_ticket_id": self.id,
+            "default_ticket_ids": [(4, [self.id])],
             "default_partner_id": self.partner_id.id,
         }
         return action
