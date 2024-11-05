4aacb6a3d27c8fcafe4529c4cd6dc660bd4bb7ac
ilo <dev@nomadiplus.com>
2024-11-05 12:23:00 -0300

    [17.0][MIG] helpdesk_mgmt_fieldservice: Migration to 17.0

--- helpdesk_mgmt_fieldservice/models/res_partner.py
+++ helpdesk_mgmt_fieldservice/models/res_partner.py
@@ -10,10 +10,10 @@ class ResPartner(models.Model):
         ) and self.env.user.has_group("helpdesk_mgmt.group_helpdesk_user_own"):
             context = dict(self.env.context)
             context.pop("search_default_open", None)
-            self = self.with_context(
+            context_self = self.with_context(
                 context,
                 default_partner_id=self.id,
                 default_fsm_location_id=self.service_location_id.id,
             )
-            return super(ResPartner, self).action_view_helpdesk_tickets()
-        return super().action_view_helpdesk_tickets
+            return super(ResPartner, context_self).action_view_helpdesk_tickets()
+        return super().action_view_helpdesk_tickets()
