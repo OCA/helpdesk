20b92943c1895a3f36e73366de52b68717cbc230
kobros-tech <alkobroslymohamed@gmail.com>
2025-01-23 20:25:24 +0300

    [18.0][FIX] helpdesk_mgmt: update kanban content and images

--- helpdesk_mgmt/__manifest__.py
+++ helpdesk_mgmt/__manifest__.py
@@ -39,7 +39,7 @@
             "helpdesk_mgmt/static/src/js/new_ticket.esm.js",
         ],
     },
-    "development_status": "Beta",
+    "development_status": "Production/Stable",
     "application": True,
     "installable": True,
 }
