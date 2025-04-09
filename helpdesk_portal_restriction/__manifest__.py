89a3c117cc3f9fb8c18f163158572f258cc7959e
Lansana Barry Sow <lbarry@apsl.net>
2025-04-09 16:10:17 +0200

    [MIG] helpdesk_portal_restriction: Migration to 17.0

--- helpdesk_portal_restriction/__manifest__.py
+++ helpdesk_portal_restriction/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
     "name": "Helpdesk Portal Restriction",
-    "version": "16.0.1.0.0",
+    "version": "17.0.1.0.0",
     "category": "Helpdesk",
     "website": "https://github.com/OCA/helpdesk",
     "author": "Lansana Barry Sow, APSL-Nagarro, Odoo Community Association (OCA)",
