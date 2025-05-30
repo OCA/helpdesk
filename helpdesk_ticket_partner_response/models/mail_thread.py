fa6343012e97dd7d9d11c78664ba63c82f44d6bf
Anjeel Haria <bytemeasap@gmail.com>
2025-05-30 10:26:01 +0530

    [MIG] helpdesk_ticket_partner_response: Migration to 18.0
    Updated test scripts

--- helpdesk_ticket_partner_response/models/mail_thread.py
+++ helpdesk_ticket_partner_response/models/mail_thread.py
@@ -12,16 +12,22 @@ class MailThread(models.AbstractModel):
     def change_status_ticket_from_portal(self, routes):
         if routes and routes[0][0] == "helpdesk.ticket":
             ticket_id = routes[0][1]
-            ticket = self.env["helpdesk.ticket"].sudo().browse(int(ticket_id))
-            partner_id = (
-                self.env["res.users"]
-                .search([("id", "=", routes[0][3])], limit=1)
-                .partner_id.id
-            )
-            if (
-                ticket
-                and partner_id == ticket.partner_id.id
-                and ticket.team_id.autoupdate_ticket_stage
-                and ticket.stage_id in ticket.team_id.autopupdate_src_stage_ids
-            ):
-                ticket.stage_id = ticket.team_id.autopupdate_dest_stage_id.id
+            if ticket_id:
+                ticket = self.env["helpdesk.ticket"].sudo().browse(int(ticket_id))
+                if ticket and routes[0][3]:
+                    partner_id = (
+                        self.env["res.users"]
+                        .search([("id", "=", routes[0][3])], limit=1)
+                        .partner_id.id
+                    )
+                    if partner_id:
+                        if (
+                            ticket
+                            and partner_id == ticket.partner_id.id
+                            and ticket.team_id.autoupdate_ticket_stage
+                            and ticket.stage_id
+                            in ticket.team_id.autopupdate_src_stage_ids
+                        ):
+                            ticket.stage_id = (
+                                ticket.team_id.autopupdate_dest_stage_id.id
+                            )
