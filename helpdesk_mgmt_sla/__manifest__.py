1dfc9a40777e21a0a3e142b07b47961d49d1e8da
David Bañón <david.banon@tecnativa.com>
2025-11-10 11:05:36 +0100

    [IMP] helpdesk_mgmt_sla: Add company_id to helpdesk.sla
    TT56371

--- helpdesk_mgmt_sla/__manifest__.py
+++ helpdesk_mgmt_sla/__manifest__.py
@@ -10,6 +10,7 @@
     "version": "17.0.2.0.0",
     "depends": ["base", "helpdesk_mgmt", "resource"],
     "data": [
+        "security/helpdesk_sla_security.xml",
         "views/helpdesk_sla_report.xml",
         "views/helpdesk_ticket_sla.xml",
         "security/ir.model.access.csv",
