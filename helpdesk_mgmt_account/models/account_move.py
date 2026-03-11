6eb811cba56c863f40b969f3330e460164049ccc
Nicolas Delbovier <nicolas.delbovier@acsone.eu>
2026-03-11 14:49:24 +0100

    [REF] helpdesk_mgmt_account: move open tickets action to Python Shifting the action logic to Python facilitates dynamic customization. This approach allows inheriting modules to override or extend the action via super(), avoiding the limitations and complexity of XML xpath inheritance for conditional logic

--- helpdesk_mgmt_account/models/account_move.py
+++ helpdesk_mgmt_account/models/account_move.py
@@ -1,7 +1,7 @@
 # Copyright 2025 ACSONE SA/NV
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
-from odoo import api, fields, models
+from odoo import Command, api, fields, models
 
 
 class AccountMove(models.Model):
@@ -17,3 +17,16 @@ class AccountMove(models.Model):
     def _compute_ticket_count(self):
         for move in self:
             move.ticket_count = len(move.ticket_ids)
+
+    def action_view_helpdesk_tickets(self):
+        self.ensure_one()
+        action = self.env["ir.actions.act_window"]._for_xml_id(
+            "helpdesk_mgmt.helpdesk_ticket_action"
+        )
+        action["domain"] = [("account_move_ids", "in", [self.id])]
+        action["context"] = {
+            "default_account_move_ids": [Command.set(self.ids)],
+            # ↓ enables to show the "1" in smart button even before save
+            "default_account_move_count": 1,
+        }
+        return action
