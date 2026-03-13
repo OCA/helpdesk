6313f23f57a47331045059449a642b7b7146dd5c
Nicolas Delbovier <nicolas.delbovier@acsone.eu>
2026-03-13 10:54:09 +0100

    [ADD] helpdesk_mgmt_stock_purchase_origin: Pre-fill purchase order field on helpdesk ticket from an incoming picking

---
+++ helpdesk_mgmt_stock_purchase_origin/models/stock_picking.py
@@ -0,0 +1,28 @@
+# Copyright 2026 ACSONE SA/NV
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+from odoo import Command, models
+
+
+class StockPicking(models.Model):
+
+    _inherit = "stock.picking"
+
+    def action_view_helpdesk_tickets(self):
+        action = super().action_view_helpdesk_tickets()
+
+        picking_type_code = self.picking_type_id.code
+        if picking_type_code == "incoming":
+            origin_po_data = self.env["purchase.order"].search_read(
+                domain=[("name", "=", self.origin)], fields=["id"], limit=1
+            )
+            if origin_po_data:
+                action["context"].update(
+                    {
+                        "default_purchase_order_ids": [
+                            Command.set([origin_po_data[0].get("id")])
+                        ]
+                    }
+                )
+
+        return action
