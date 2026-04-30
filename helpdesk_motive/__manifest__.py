b6fe851515a0eb52ac1543d00225f41ea29c016a
meetscs <meet.p.serpentcs@gmail.com>
2026-04-30 12:44:55 +0530

    [IMP] helpdesk_type: pre-commit auto fixes

--- helpdesk_motive/__manifest__.py
+++ helpdesk_motive/__manifest__.py
@@ -5,7 +5,7 @@
 
 {
     "name": "Helpdesk Motive",
-    "version": "18.0.1.0.0",
+    "version": "19.0.1.0.0",
     "license": "AGPL-3",
     "summary": "Keep the motive ",
     "author": "Binhex, Konos, Open Source Integrators, "
