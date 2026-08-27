5524783b37564beab4d5564d4e4a6b4a90fd5d99
Víctor Martínez <victor.martinez@tecnativa.com>
2026-08-27 14:35:34 +0200

    [IMP] helpdesk_mgmt_sale: Remove the unnecessary order linking wizard
    The wizard is unnecessary because it is currently too restrictive (https://github.com/OCA/helpdesk/pull/1019#pullrequestreview-4400851976) and because this can already be done (defining orders) directly in the ticket field itself.

--- helpdesk_mgmt_sale/__manifest__.py
+++ helpdesk_mgmt_sale/__manifest__.py
@@ -3,17 +3,15 @@
 {
     "name": "Helpdesk Sale Order",
     "summary": "Add the option to select project in the sale orders.",
-    "version": "18.0.2.1.1",
+    "version": "18.0.3.0.0",
     "license": "AGPL-3",
     "category": "Sales Management",
     "author": "Tecnativa," "Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/helpdesk",
     "depends": ["helpdesk_mgmt", "sale"],
     "data": [
-        "security/ir.model.access.csv",
         "views/helpdesk_ticket_views.xml",
         "views/sale_order_views.xml",
-        "wizards/helpdesk_ticket_link_sale_order.xml",
     ],
     "development_status": "Production/Stable",
     "auto_install": True,
